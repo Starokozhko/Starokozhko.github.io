933401ab5fff3cd2bcdc80bc24bd4972bcd44232
Starokozhko <jcat84@mail.ru>
2016-06-16 12:52:03 +0300

    add  correction

--- js_1-2_dz/js/script.js
+++ js_1-2_dz/js/script.js
@@ -3,37 +3,65 @@ function first() {
 	var text2 = 'Укажите в какую степень возводить число';
 
 	var numb = isNan(text1);
-	var power = isNan(text2);
+	if (numb != null) {
+		var power = isNan(text2);
+	}
 
-	console.log (pow(numb, power));
+	if (numb != null && power != null) {		
+		console.log (pow(numb, power));
+	}
 
 	function isNan(text){
-		var numb;
-		while (isNaN( numb )) {
-			numb = prompt( text );
+		var number;
+
+		while (isNaN( number )) {
+			number = prompt( text );
+
+			if (number == null) return;
+
+			while (number == '') {
+				number = prompt( 'Вы ничего не ввели! Введите число возводимое в степень!' );
+			}
 		}
 
-		return numb;
+		return number;
 	}
 
 	function pow(a, n) {
-		var result = a;
-		
-		for (var i = n; i > 1; i--) {
-			result *= a;
-		}
 
+		var result = a;
+
+		if (n > 0){
+			for (var i = n; i > 1; i--) {
+				result *= a;
+			}
+		} else {
+			for (var i = n; i < -1; i++) {
+				result *= a;
+			}
+			result = 1 / result;
+		}
 		return result;
 	}
 }
 
 function second() {
+
 	var arr = [];
+
 	for (var i = 5; i > 0; i--){
-		arr.push(prompt('Введите ' + i + ' любых имен. По одному!'));
+		var userName  = prompt('Введите ' + i + ' любых имен. По одному!');
+		if(userName == null) return;
+		while (userName == ''){
+			userName = prompt('Введите ' + i + ' любых имен. По одному!');
+			if(userName == null) return;
+		}
+		arr[i] = userName;
 	}
 	
 	var usernam = prompt('Введите имя пользователя!');
+	if(usernam == null) return;
+
 	var authorized = 'none';
 
 	for(var i = 0; i < arr.length; i++) {
@@ -49,5 +77,5 @@ function second() {
 		alert('Ошибка!!!');
 	}
 
-	// console.log(arr);
+	console.log(arr);
 }
