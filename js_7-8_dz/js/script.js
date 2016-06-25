246b5a877f2c1e221ae65bed72bd1ee828dc0c83
Starokozhko <jcat84@mail.ru>
2016-06-25 04:16:21 +0300

    third correction

--- js_7-8_dz/js/script.js
+++ js_7-8_dz/js/script.js
@@ -7,12 +7,34 @@ $(function(){
 
 //Код для второго задания
 	var $inputs = $('input');
+
+	var $mytitle = {
+		firstName: $('input[name="firstName"]').attr('title'),
+		lastName: $('input[name="lastName"]').attr('title'),
+		address: $('input[name="address"]').attr('title')
+	};
+	var $mydiv1 = $('<div>', {
+		class: 'helpd',
+		text: $mytitle.firstName
+	});
+	var $mydiv2 = $('<div>', {
+		class: 'helpd',
+		text: $mytitle.lastName
+	});
+	var $mydiv3 = $('<div>', {
+		class: 'helpd',
+		text: $mytitle.address
+	});
+	// console.log($mytitle.first);
 	// =====================================
 	
-	$inputs.hover(sowHelpd,hideHelpd);
+	 $inputs.hover(sowHelpd,hideHelpd);
 	var $button = $('button');
 
 	$button.on('click', function(){
+		$('input[name="firstName"]').parent().append($mydiv1);
+		$('input[name="lastName"]').parent().append($mydiv2);
+		$('input[name="address"]').parent().append($mydiv3);
 		$('div.helpd').animate({ opacity: 1, marginRight: '10px'}, 'slow');
 	});
 
@@ -37,24 +59,35 @@ $(function(){
 //Функция для второго задания
 	function sowHelpd() {
 		
-		var $title = $(this).attr('title');
-		
-		var $mydiv = $('<div>', {
-			class: 'helpd',
-			text: $title
-		});
-		
-		$(this).parent().append($mydiv);
-		$('.helpd').animate({ opacity: 1, marginRight: '10px'}, 'slow');
+		var $input = $(this).attr('name');
+		switch ($input) {
+			case 'firstName': $titles = $mytitle.firstName;
+												$(this).parent().append($mydiv1);
+				break;
+			case 'lastName': "value", $titles = $mytitle.lastName;
+											$(this).parent().append($mydiv2);
+				break;	
+			case 'address': $titles = $mytitle.address;
+											$(this).parent().append($mydiv3);
+				break;
+		}
+	
+		$('.helpd').animate({ opacity: 1, marginRight: '10px'}, 500);
 		$(this).attr('title','');
 	} 
+
+
+
+
+
+
 
 //Функция для второго задания
 	function hideHelpd() {
 		var $texts = $('.helpd').text();
 
 		$(this).attr('title', $texts);
-		$('.helpd').animate({ opacity: 0, marginRight: '0px'}, 'slow', function() {$('.helpd').remove()});
+		$('.helpd').animate({ opacity: 0, marginRight: '0px'}, 1, function() {$('.helpd').remove()});
 	}
 
 
